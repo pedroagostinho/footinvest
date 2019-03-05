489298c1084f7e94d39828efb3c2ec101f824bf7
Miguel Bernal <mdverabernal@gmail.com>
2019-03-05 15:14:35 +0000

    seeding testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+Club.destroy_all
+
+CSV_OPTIONS = {
+  col_sep: ',',
+  quote_char: '"',
+  headers: :first_row,
+  header_converters: :symbol
+}
+
+CSV.foreach('clubs', CSV_OPTIONS) do |row|
+  club = Club.new(
+    name: row[0],
+    logo: row[1],
+    city: row[2]
+    )
+end
