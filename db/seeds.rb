f239bff44cefd31f6dd726f3003d85604d550360
Miguel Bernal <mdverabernal@gmail.com>
2019-03-05 16:06:59 +0000

    seeds ready to merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,137 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
+require 'byebug'
 
 Club.destroy_all
+Competition.destroy_all
+New.destroy_all
 
 CSV_OPTIONS = {
-  col_sep: ',',
+  col_sep: ';',
   quote_char: '"',
   headers: :first_row,
   header_converters: :symbol
 }
 
-CSV.foreach('clubs', CSV_OPTIONS) do |row|
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CSV.foreach('./db/clubs.csv', CSV_OPTIONS) do |row|
   club = Club.new(
     name: row[0],
     logo: row[1],
     city: row[2]
     )
+  club.save
+end
+
+CSV.foreach('./db/competitions.csv', CSV_OPTIONS) do |row|
+  competition = Competition.new(
+    name: row[0],
+    logo: row[1]
+    )
+  competition.save
+end
+
+CSV.foreach('./db/news.csv', CSV_OPTIONS) do |row|
+  byebug
+  noticia = New.new(
+    date_time: row[0],
+    tag: row[1],
+    title: row[2],
+    summary: row[3],
+    content: row[4],
+    club: Club.find(row[5].to_i),
+    player: Player.find(ow[6].to_i)
+    )
+  noticia.save
+end
+
+CSV.foreach('./db/results.csv', CSV_OPTIONS) do |row|
+  result = Result.new(
+    date_time: row[0],
+    competition_id: row[1],
+    home_club_goals: row[2]
+    away_club_goals: row[3],
+    home_club_id: row[4].to_i,
+    home_club_id: row[5].to_i
+    )
+  resutl.save
 end
