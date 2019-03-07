befc773941cf2a818e66f60ca68b3af402572108
Miguel Bernal <mdverabernal@gmail.com>
2019-03-07 14:29:04 +0000

    market with no css yet

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,11 +1,20 @@
 class PlayersController < ApplicationController
 
-
   def index
-    if (params[:name].present? || params[:tokens].present?)
+    @players = []
+
+    if params[:name].present?
       # @players = Player.global_search(params[:query])
       @players = Player.where("name ILIKE ?", "%#{params[:name]}%")
-      @players = Token.where(owner: nil).count <= params[:token]
+      respond_to do |format|
+        format.html { redirect_to players_path }
+        format.js  # <-- will render `app/views/players/index.js.erb#.where.not(club_id: current_club.id)
+      end
+
+    elsif params[:tokens].present?
+      Player.all.each do |pl|
+        @players << pl if pl.tokens.where(on_sale: true).count == params[:tokens].to_i
+      end
 
       respond_to do |format|
         format.html { redirect_to players_path }
@@ -13,11 +22,13 @@ class PlayersController < ApplicationController
       end
     else
       @players = Player.all #= Player.where.not(club_id: current_club.id, availability: false)
+
       respond_to do |format|
         format.html { render 'players/index' }
         format.js  # <-- idem
       end
     end
+
   end
 
   def show
