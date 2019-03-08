545ec1d95cf353f0b071338fd47ea9edfdacb0b2
Pedro Agostinho <pedroagosti@gmail.com>
2019-03-08 16:17:23 +0000

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,9 @@ User.destroy_all
 Player.destroy_all
 Club.destroy_all
 
+
+
+
 CSV_OPTIONS = {
   col_sep: ';',
   quote_char: '"',
@@ -40,6 +43,7 @@ CSV.foreach('./db/clubs.csv', CSV_OPTIONS) do |row|
   club.save
 end
 
+
 CSV.foreach('./db/players.csv', CSV_OPTIONS) do |row|
   player = Player.new(
     name: row[0],
@@ -47,7 +51,7 @@ CSV.foreach('./db/players.csv', CSV_OPTIONS) do |row|
     age: row[2],
     height: row[3],
     nationality: row[4],
-    club_id: row[5],
+    club_id: find_club,
     position: row[6],
     social_url: row[7]
     )
@@ -75,6 +79,8 @@ CSV.foreach('./db/results.csv', CSV_OPTIONS) do |row|
 end
 
 CSV.foreach('./db/stats.csv', CSV_OPTIONS) do |row|
+  jf = User.find_by(name: row[0])
+
   stat = Stat.new(
     player_id: row[0],
     competition_id: row[1],
@@ -136,3 +142,11 @@ CSV.foreach('./db/transactions.csv', CSV_OPTIONS) do |row|
     )
   transaction.save
 end
+
+
+
+
+
+
+
+
