b0b62608ca990044a94e6822188e60e43c533fef
Pedro Agostinho <pedroagosti@gmail.com>
2019-03-10 18:08:00 +0000

    my players

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_190417) do
+ActiveRecord::Schema.define(version: 2019_03_10_180601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,6 +134,7 @@ ActiveRecord::Schema.define(version: 2019_03_05_190417) do
     t.datetime "birthday"
     t.string "first_name"
     t.string "last_name"
+    t.integer "balance"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
