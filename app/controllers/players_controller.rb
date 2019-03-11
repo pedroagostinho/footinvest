2ff8e8280fcb692d3c80ab8bffe343ba74c28c29
olserra <olserra@gmail.com>
2019-03-11 15:39:07 +0000

    Personalized player show

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -36,11 +36,24 @@ class PlayersController < ApplicationController
         format.js  # <-- idem
       end
     end
-
   end
 
   def show
     @player = Player.find(params[:id])
-  end
 
+    sorted = Token.joins(:transactions).select('tokens.id, tokens.player_id, transactions.date_time, transactions.price').order('transactions.date_time DESC, tokens.player_id ASC')
+
+    player_tokens = sorted.where(player_id: @player.id)
+    @variation = (((player_tokens.first.price - player_tokens.second.price) / player_tokens.second.price.to_f) * 100).round(2)
+
+    tokens_with_own = Token.where(player_id: params[:id], on_sale: true)
+    @tokens = tokens_with_own.reject { |token| token.owner == current_user.id }
+
+    @token_last_price = tokens_with_own.order(last_price: :DESC).last
+  end
 end
+
+
+
+
+
